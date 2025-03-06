4c7ce87da7d8a2cea470680e3ed0038447eae692
WizzOFF <maher_off@proton.me>
2025-03-06 14:50:07 +0100

    break_program_into_functions.c: assign multiple parameters to a function

--- break_program_into_functions.c
+++ break_program_into_functions.c
@@ -1,7 +1,14 @@
-int triple(int nombre)
+/*int triple(int nombre)
 {
     int resultat = 0;
 
     resultat = 3 * nombre;  // On multiplie le nombre fourni par 3
     return resultat;       // On retourne la variable resultat qui vaut le triple de nombre
+}
+*/
+
+
+int addition(int a, int b)
+{
+    return a + b;
 }

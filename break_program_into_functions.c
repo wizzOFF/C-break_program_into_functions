c57c2bf65cc3bcafd453b66a060f22d78a315e3e
WizzOFF <maher_off@proton.me>
2025-03-06 15:35:55 +0100

    break_program_into_functions.c: call a function

--- break_program_into_functions.c
+++ break_program_into_functions.c
@@ -14,8 +14,31 @@ int addition(int a, int b)
 }
 */
 
-
+/*
 void bonjour()
 {
     printf("Bonjour");
+}
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int triple(int nombre)
+{
+    return 3 * nombre;
+}    
+
+int main(int argc, char *argv[])
+{
+    int nombreEntre = 0, nombreTriple = 0;
+    
+    printf("Entrez un nombre... ");
+    scanf("%d", &nombreEntre);
+    
+    nombreTriple = triple(nombreEntre);
+    printf("Le triple de ce nombre est %d\n", nombreTriple);
+    
+    return 0;
 }

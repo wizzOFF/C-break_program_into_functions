811176f098aa421e9d4be19bc119960033960458
WizzOFF <maher_off@proton.me>
2025-03-06 16:22:24 +0100

    break_program_into_functions.c: understand the reading direction of a function

--- break_program_into_functions.c
+++ break_program_into_functions.c
@@ -21,6 +21,27 @@ void bonjour()
 }
 */
 
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int triple(int nombre)
+{
+    return 3 * nombre;
+}    
+
+int main(int argc, char *argv[])
+{
+    int nombreEntre = 0, nombreTriple = 0;
+    
+    printf("Entrez un nombre... ");
+    scanf("%d", &nombreEntre);
+    
+    nombreTriple = triple(nombreEntre);
+    printf("Le triple de ce nombre est %d\n", nombreTriple);
+}
+*/
+
 
 #include <stdio.h>
 #include <stdlib.h>

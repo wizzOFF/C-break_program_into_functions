e1aca80ae84f227044d85aa27fee23ce702d53dd
WizzOFF <maher_off@proton.me>
2025-03-06 17:09:49 +0100

    break_program_into_functions.c: function takes no input parameters

--- break_program_into_functions.c
+++ break_program_into_functions.c
@@ -83,7 +83,7 @@ int main(int argc, char *argv[])
 }
 */
 
-
+/*
 double aireRectangle(double largeur, double hauteur)
 {
     return largeur * hauteur;
@@ -94,6 +94,45 @@ int main(int argc, char *argv[])
     printf("Rectangle de largeur 5 et hauteur 10. Aire = %f\n", aireRectangle(5, 10));
     printf("Rectangle de largeur 2.5 et hauteur 3.5. Aire = %f\n", aireRectangle(2.5, 3.5));
     printf("Rectangle de largeur 4.2 et hauteur 9.7. Aire = %f\n", aireRectangle(4.2, 9.7));
+}
+*/
+
+
+int menu()
+{
+    int choix = 0;
     
+    while (choix < 1 || choix > 4)
+    {
+        printf("Menu :\n");
+        printf("1 : Poulet de dinde aux escargots rotis a la sauce bearnaise\n");
+        printf("2 : Concombres sucres a la sauce de myrtilles enrobee de chocolat\n");
+        printf("3 : Escalope de kangourou saignante et sa gelee aux fraises poivree\n");
+        printf("4 : La surprise du Chef (j'en salive d'avance...)\n");
+        printf("Votre choix ? ");
+        scanf("%d", &choix);
+    }    
+    
+    return choix;
+}    
+
+int main(int argc, char *argv[])
+{    
+    switch (menu())
+    {
+        case 1:
+            printf("Vous avez pris le poulet\n");
+            break;
+        case 2:
+            printf("Vous avez pris les concombres\n");
+            break;
+        case 3:
+            printf("Vous avez pris l'escalope\n");
+            break;
+        case 4:
+            printf("Vous avez pris la surprise du Chef. Vous etes un sacre aventurier dites donc !\n");
+            break;    
+    }
+
     return 0;
 }
